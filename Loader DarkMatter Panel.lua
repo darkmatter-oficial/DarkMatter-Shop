64f6349979aa92cee4fa7fca897659dbd6ede520
darkmatter-oficial <oficialdarkmatter@gmail.com>
2026-04-02 13:22:24 -0500

    Update Loader DarkMatter Panel.lua

--- Loader DarkMatter Panel.lua
+++ Loader DarkMatter Panel.lua
@@ -3,10 +3,10 @@
 -- ==========================================
 -- Reemplaza los enlaces a continuación por los enlaces "Raw" de tus archivos en GitHub.
 
-local urlPart1 = "URL_AQUI_PARTE_1_RAW"
-local urlPart2 = "URL_AQUI_PARTE_2_RAW"
-local urlPart3 = "URL_AQUI_PARTE_3_RAW"
-local urlPart4 = "URL_AQUI_PARTE_4_RAW"
+local urlPart1 = "https://raw.githubusercontent.com/darkmatter-oficial/DarkMatter-Shop/refs/heads/main/Variables%20y%20Estado.lua"
+local urlPart2 = "https://raw.githubusercontent.com/darkmatter-oficial/DarkMatter-Shop/refs/heads/main/Interfaz%20de%20Usuario.lua"
+local urlPart3 = "https://raw.githubusercontent.com/darkmatter-oficial/DarkMatter-Shop/refs/heads/main/Configuraci%C3%B3n%20de%20Men%C3%BA.lua"
+local urlPart4 = "https://raw.githubusercontent.com/darkmatter-oficial/DarkMatter-Shop/refs/heads/main/Sistema%20Visuales%20y%20De%20Bucle%20Principal.lua"
 
 -- Descarga y une todas las partes en un solo script continuo
 local scriptCompleto = game:HttpGet(urlPart1) .. "\n" .. 
